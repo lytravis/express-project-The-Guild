d4fcdc81cf6248842201a80c4aee1b755a248774
Nick Rogers <nickjrogers@gmail.com>
2021-10-21 14:06:05 -0700

    fixed game page when signed out

--- routes/games.js
+++ routes/games.js
@@ -18,20 +18,30 @@ router.get("/", asyncHandler(async (req, res) => {
 router.get(
   "/:gameId(\\d+)",
   asyncHandler(async(req, res) => {
-    const userId = req.session.auth.userId;
-    const gameshelves = await GameShelf.findAll({
-      include: [{
-        model: User,
-        where: { id: userId }
-      }]
-    })
+    if (req.session.auth){
+      console.log('!!!!!!!!!')
+      const userId = req.session.auth.userId;
+      const gameshelves = await GameShelf.findAll({
+        include: [{
+          model: User,
+          where: { id: userId }
+        }]
+      })
 
-    const id = req.params.gameId;
-    const game = await Game.findByPk(id);
-    const reviews = await Review.findAll({
-      where: {gameId: id}
-    })
-    res.render("one-game-page", {game, reviews, userId, gameshelves})
+      const id = req.params.gameId;
+      const game = await Game.findByPk(id);
+      const reviews = await Review.findAll({
+        where: {gameId: id}
+      })
+      res.render("one-game-page", {game, reviews, userId, gameshelves})
+    } else {
+      const id = req.params.gameId;
+      const game = await Game.findByPk(id);
+      const reviews = await Review.findAll({
+        where: {gameId: id}
+      })
+      res.render("one-game-page", {game, reviews})
+    }
 }));
 
 module.exports = router;

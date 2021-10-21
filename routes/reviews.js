7f4358d3cb31cd5935529b7c4a14749b3e42cc72
mehendaleo <mehendaleomkar@gmail.com>
2021-10-21 15:30:29 -0400

    work so far

--- routes/reviews.js
+++ routes/reviews.js
@@ -18,7 +18,6 @@ router.post('/:gameId(\\d+)', csrfProtection, asyncHandler(async(req, res)=>{
     const gameId = req.params.gameId;
     console.log(gameId)
     const { rating, content } = req.body;
-    // const game = await Game.findByPk(userId);
     await Review.create({
         userId,
         gameId,
@@ -29,12 +28,14 @@ router.post('/:gameId(\\d+)', csrfProtection, asyncHandler(async(req, res)=>{
 }));
 
 // get form to update specific review
-router.get('/:gameId(\\d+)/:reviewId', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
-    const reviewId = req.params.id;
+router.get('/:gameId(\\d+)/:reviewId(\\d+)', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
+    const gameId = req.params.gameId;
+    const reviewId = req.params.reviewId;
+    const game = await Game.findByPk(gameId);
     const userId = req.session.auth.userId;
     const review = await Review.findByPk(reviewId);
     if (review.userId === userId) {
-        res.render('review-form', {csrfToken: req.csrfToken()})
+        res.render('update-review', {csrfToken: req.csrfToken(), game, gameId, reviewId});
     } else {
         res.send('Please log in as the owner of this review');
         res.redirect('/users/login');
@@ -42,11 +43,11 @@ router.get('/:gameId(\\d+)/:reviewId', requireAuth, csrfProtection, asyncHandler
 }))
 
 // patch for specific review for specific game
+// still needs work
 router.post('/:id(\\d+)/update', csrfProtection, asyncHandler(async(req, res) => {
     requireAuth(req, res, next);
     const reviewId = req.params.id;
     const userId = req.session.auth.userId;
-
 }))
 
 module.exports = router;

9d684ced21997409b24f158c70d5a8f6857b4a87
Nick Rogers <nickjrogers@gmail.com>
2022-02-16 18:06:00 -0800

    finished debugging modal rendering

--- public/javascripts/comment.js
+++ public/javascripts/comment.js
@@ -1,27 +1,11 @@
 window.addEventListener("load", (event) => {
-  let modalBtn;
-  let modal;
-
-  function windowOnClick(event) {
-    if (event.currentTarget === modal) toggleModal()
-  }
-
-  function toggleModal() {
-    modal.classList.toggle("show-modal");
-  }
 
   function getModal(event) {
     let index = event.target.classList[1];
-    modalBtn = event.target;
-    modal = document.querySelectorAll(`.${index}`)[1];
-    return toggleModal();
+    let modal = document.querySelectorAll(`.${index}`)[1];
+    if (modal) modal.classList.toggle("show-modal");
   }
 
-  console.log(modal)
-
   document.addEventListener("click", getModal)
-
-  // modalBtn.addEventListener("click", toggleModal);
-  window.addEventListener("click", windowOnClick);
 
 });

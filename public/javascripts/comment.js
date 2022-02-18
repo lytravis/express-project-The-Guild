0798572c5de6c9d87f0a7c0934e852e28a7f71f8
Nick Rogers <nickjrogers@gmail.com>
2022-02-17 17:09:12 -0800

    debugging update review

--- public/javascripts/comment.js
+++ public/javascripts/comment.js
@@ -1,11 +1,35 @@
 window.addEventListener("load", (event) => {
-
-  function getModal(event) {
-    let index = event.target.classList[1];
-    let modal = document.querySelectorAll(`.${index}`)[1];
+  const form = document.querySelector('.update-comment-form');
+  async function getModal(event) {
+    const index = event.target.classList[1];
+    const modal = document.querySelectorAll(`.${index}`)[1];
     if (modal) modal.classList.toggle("show-modal");
   }
 
   document.addEventListener("click", getModal);
+
+
+
+  const comment = document.querySelector('.update-comment-button');
+
+  form.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    const content = form.elements['content'].value;
+    const rating = +form.elements['rating'].value;
+    const token = form.elements['_csrf'].value;
+
+    const res = await fetch(`/${form.action.split('/').slice(3).join('/')}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'CSRF-Token': `${token}`
+      },
+      body: JSON.stringify({
+        rating,
+        content
+      })
+    });
+    return res;
+  })
 
 });
